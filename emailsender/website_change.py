77ab10fd14e0b70e43a6cc78580ecd8ea97380af
nick-ras <nick-ras@mail.dk>
2023-03-14 12:36:08 +0100

    changes in emailsender

--- emailsender/website_change.py
+++ emailsender/website_change.py
@@ -2,38 +2,34 @@ from datetime import datetime
 import time
 import hashlib
 from urllib.request import urlopen, Request
-from developit import emails
+from emailsender import emails
+import os
+import requests
 
 class website_check():
 	def check_changes(self):
-		url = Request('https://youtube.com/',
-									headers={'User-Agent': 'Mozilla/5.0'})
-		response = urlopen(url).read()
-		currentHash = hashlib.sha224(response).hexdigest()
-		print("running")
-		time.sleep(10)
+		url = requests.get('https://www.dr.dk')
+		hash = hashlib.sha256()
+		hash.update(url.text.encode('utf-8'))
 		while True:
+			print("running")
 			try:
-				# perform the get request and store it in a var
-				response = urlopen(url).read()
-				currentHash = hashlib.sha224(response).hexdigest()
-				time.sleep(30)
-				response = urlopen(url).read()
-				newHash = hashlib.sha224(response).hexdigest()
-				if newHash == currentHash:
-					continue
-				else:
+				url2 = requests.get('https://www.dr.dk')
+				hash2 = hashlib.sha256()
+				hash2.update(url2.text.encode('utf-8'))
+				if (hash.hexdigest() != hash2.hexdigest()):
+					print("changed")
 					emails().send_welcome_email()
-					response = urlopen(url).read()
-					currentHash = hashlib.sha224(response).hexdigest()
-					time.sleep(2)
-					continue
+					hash = hash2
+				else:
+					print("not changed")
 			except Exception as e:
-					print("error")
+				print("error")
+				exit(2)
+			time.sleep(2)
+		return response
 
 check = website_check().check_changes()
 
-print("hello world")
-print(os.environ)
-check()
+print(check)
 
